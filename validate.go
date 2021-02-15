59d3201796baa8eb0e89425a6eb90ef8d9ae1a73
ichsanputr <githubichsan@gmail.com>
2021-02-15 10:11:47 +0700

    trim code validate

--- validate.go
+++ validate.go
@@ -2,18 +2,28 @@ package passgen
 
 import "errors"
 
-var (count int)
+var (
+	count int
+)
 
-func ValidatePass(input int,l bool,u bool,n bool,s bool) error {
-	
-	if l {count++}
-   if u {count++}
-	if n {count++}
-	if s {count++}
+func ValidatePass(input int, l bool, u bool, n bool, s bool) error {
 
-	if input == 0 || count == 0 || input < count{
+	if l {
+		count++
+	}
+	if u {
+		count++
+	}
+	if n {
+		count++
+	}
+	if s {
+		count++
+	}
+
+	if input == 0 || count == 0 || input < count {
 		// return error with erros.New
-		// errors.New for return error with string 
+		// errors.New for return error with string
 		// 2 option for type error = nil or error.New
 		// except use error.New can use fmt.Errorf()
 		return errors.New("length must not be less than the number of requirements || length and terms cannot be empty")
